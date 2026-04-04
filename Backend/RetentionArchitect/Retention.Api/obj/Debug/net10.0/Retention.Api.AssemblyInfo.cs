1b25ac0abe7fa11e6b671614948ce722f37d3986
Daniyar Kshibekov <danik30032005@gmail.com>
2026-04-04 15:12:56 +0500

    1

--- Backend/RetentionArchitect/Retention.Api/obj/Debug/net10.0/Retention.Api.AssemblyInfo.cs
+++ Backend/RetentionArchitect/Retention.Api/obj/Debug/net10.0/Retention.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Retention.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbb2091af69172ca040dac99c93bad64195041fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1051ce911c64c7c03ef5d46a98847edc75ed915b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Retention.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Retention.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
