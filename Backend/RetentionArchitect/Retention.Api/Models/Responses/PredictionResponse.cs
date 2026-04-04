a2c616daa9f12b0d1096d36f9540908ca98937f1
Daniyar Kshibekov <danik30032005@gmail.com>
2026-04-04 19:05:05 +0500

    1

---
+++ Backend/RetentionArchitect/Retention.Api/Models/Responses/PredictionResponse.cs
@@ -0,0 +1,10 @@
+namespace Retention.Api.Models.Responses;
+
+public class PredicitonResponse
+{
+    public string UserId { get; set; } = string.Empty;
+    public double ChurnProbability { get; set; }
+    public string PredictedClass { get; set; } = string.Empty;
+    public List<string> TopDrivers { get; set; } = new();
+    public List<string> RecommendedActions { get; set; } = new();
+}
